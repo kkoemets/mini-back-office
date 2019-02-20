dde3d2868743a196e74e5a0e5a6e183437249736
Kristjan Koemets <36334709+Jollerr@users.noreply.github.com>
2019-02-20 08:51:36 +0200

    add import java.math.BigDecimal

--- src/main/java/com/pocopay/minibackoffice/controller/TransactionController.java
+++ src/main/java/com/pocopay/minibackoffice/controller/TransactionController.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Map;
 
